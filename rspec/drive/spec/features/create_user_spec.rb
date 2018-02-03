1f8639468ade44780eae25c3f80161059c36c4ca
Prashanth <prashanth@tarkalabs.com>
2018-02-03 12:35:28 +0530

    Faker change

--- rspec/drive/spec/features/create_user_spec.rb
+++ rspec/drive/spec/features/create_user_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 describe "create user", :type => :feature do
   it "creates a new user" do
     visit '/users/new'
-    fill_in 'Email', with: 'user@example.com'
-    fill_in 'Name', with: 'user'
+    fill_in 'Email', with: Faker::Internet.email
+    fill_in 'Name', with: Faker::Name.name
     click_button 'Create User'
     sleep 2
     expect(page).to have_content 'User was successfully created.'
